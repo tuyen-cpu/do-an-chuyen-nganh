87bca3167d349cb617f1b53f5ef19ac3887b227f
NguyenTrung-PM <96853789+NguyenTrung-PM@users.noreply.github.com>
2022-05-21 18:24:25 +0700

    finish feature header design

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,17 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { faSearch, faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons';
 
+interface Menu {
+    title?: string;
+    icon?: any;
+    img?:string,
+    routerLink?: string;
+}
+
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  faSearch = faSearch;
-  faShoppingBag = faBagShopping;
-  faUser = faUser;
+    faSearch = faSearch;
+    faShoppingBag = faBagShopping;
+    faUser = faUser;
 
-  constructor() {}
+    menu: Menu[] = [
+        {
+            title: 'New in',
+            routerLink: '',
+        },
+        {
+            title: 'Tất cả sản phẩm',
+            routerLink: '',
+        },
+        {
+            img: 'mybear.svg',
+            title: "That's my bear",
+            routerLink: '',
+        },
+        {
+            title: 'Cửa hàng',
+            routerLink: '',
+        },
+        {
+            title: 'Chính sách',
+            routerLink: '',
+        },
+    ];
 
-  ngOnInit(): void {}
+    menuSub: Menu[] = [
+        { icon: faSearch, routerLink: '' },
+        {
+            icon: faBagShopping,
+            routerLink: '',
+        },
+        {
+            icon: faUser,
+            routerLink: '',
+        },
+    ];
+
+    constructor() {}
+
+    ngOnInit(): void {}
 }
