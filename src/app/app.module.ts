9c0b0dc2e3be31e3c5a1eba2c551fa419ce27871
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-05-23 16:45:50 +0700

    detail product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 //primeNg
 import { ButtonModule } from 'primeng/button';
-
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ImageModule } from 'primeng/image';
 //awesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -14,10 +16,9 @@ import { HeaderComponent } from './components/shared/header/header.component';
 import { DetailProductComponent } from './components/detail-product/detail-product.component';
 
 @NgModule({
-  declarations: [AppComponent, DetailProductComponent,
-    HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, ButtonModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, DetailProductComponent, HeaderComponent],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule, ButtonModule, RadioButtonModule, ImageModule, BrowserAnimationsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
