4f6e271e9997bb7762bc31cbb34d27361010e531
vietprao2020 <deptraiphaithe258@gmail.com>
2022-08-09 20:29:29 +0700

    create component header, footer and product-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
     standalone: true,
-    imports: [RouterModule, HeaderComponent, HomeComponent],
+    imports: [RouterModule, HeaderComponent, FooterComponent, HomeComponent],
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
